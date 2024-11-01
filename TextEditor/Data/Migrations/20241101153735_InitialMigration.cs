637f189e10451676b4cbddb7a8227d10c07945b4
kkPau <karikarikenneth8@gmail.com>
2024-11-01 16:16:19 +0000

    first commit

---
+++ TextEditor/Data/Migrations/20241101153735_InitialMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextEditor.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Docs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Docs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Docs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Docs_UserId",
+                table: "Docs",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Docs");
+        }
+    }
+}
---
+++ TextEditor/Models/Doc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TextEditor.Models
+{
+	public class Doc
+	{
+		public Doc()
+		{
+		}
+
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		[Required]
+		public string? UserId { get; set; }
+
+		[ForeignKey("UserId")]
+		public IdentityUser? User { get; set; }
+	}
+}
+
